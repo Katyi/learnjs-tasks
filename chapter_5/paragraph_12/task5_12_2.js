56fb176c76cf3f46cf6058a1fadb8dbbaa3a10c9
Alexandra Egorova <egorova.aleksandra@gmail.com>
2023-02-27 11:07:30 +0200

    added file task5_12_2.js

--- chapter_5/paragraph_12/task5_12_2.js
+++ chapter_5/paragraph_12/task5_12_2.js
@@ -2,3 +2,23 @@
 Но иногда мы не можем использовать имя, так как могут быть и другие, нужные, свойства с этим именем во вложенных объектах. Поэтому можно проверять свойство по значению.
 Напишите функцию replacer для JSON-преобразования, которая удалит свойства, ссылающиеся на meetup. */
 
+let room = {
+  number: 23
+};
+
+let meetup = {
+  title: "Совещание",
+  occupiedBy: [{name: "Иванов"}, {name: "Петров"}],
+  place: room
+};
+
+room.occupiedBy = meetup;
+meetup.self = meetup;
+
+console.log( JSON.stringify(meetup, function replacer(key, value) {
+  if (key != "" && value == meetup) {
+    return undefined
+  }
+  return value;
+}));
+
